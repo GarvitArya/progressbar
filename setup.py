2b5dfb802d8a16f4c30d5f7acb4b2612465d08de
Garvit Arya <garvit.arya007@gmail.com>
2017-11-28 13:02:37 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='progressbar',
+        version='0.1',
+        description='Utility module to represent progress in the form of a progress bar',
+        url='https://github.com/GarvitArya/progressbar',
+        author='Garvit Arya',
+        author_email='garvitarya1994@gmail.com',
+        license='MIT',
+        packages=find_packages()
+)
